3ab9e3eac5e7fb3fe6e32e02eca700e59de1f4c3
GOSSIP <40517663+GOSSIP-DEV@users.noreply.github.com>
2018-07-18 17:12:17 +0200

    Update gossip-autoinstall.sh

--- gossip-autoinstall.sh
+++ gossip-autoinstall.sh
@@ -8,53 +8,47 @@ COIN_CLI='gossipcoin-cli'
 COIN_PATH='/usr/local/bin/'
 COIN_TGZ='https://github.com/g0ssipcoin/GossipCoinCore/releases/download/v1.1.0.0/Linux-gossipcoin.zip'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
-COIN_NAME='gossipcoin'
+COIN_NAME='GOSSIP'
 COIN_PORT=22123
 RPC_PORT=22122
 
 NODEIP=$(curl -s4 icanhazip.com)
 
-BLUE="\033[0;34m"
-YELLOW="\033[0;33m"
-CYAN="\033[0;36m" 
-PURPLE="\033[0;35m"
 RED='\033[0;31m'
-GREEN="\033[0;32m"
+GREEN='\033[0;32m'
 NC='\033[0m'
-MAG='\e[1;35m'
 
-purgeOldInstallation() {
-    echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
-    killall $COIN_DAEMON > /dev/null 2>&1
-    ufw delete allow $COIN_PORT/tcp > /dev/null 2>&1
+function delete_old_installation() {
+  echo -e "${GREEN}Searching and removing old $COIN_NAME files and configurations${NC}"
+  killall $COIN_DAEMON > /dev/null 2>&1
+  ufw delete allow $COIN_PORT/tcp > /dev/null 2>&1
 	rm -r .goss* > /dev/null 2>&1
 	rm -r linux > /dev/null 2>&1 
 	rm gos* > /dev/null 2>&1
-    rm $COIN_CLI $COIN_DAEMON > /dev/null 2>&1
-    if [ -d "~/.$COIN_NAME" ]; then
-        sudo rm -rf ~/.$COIN_NAME > /dev/null 2>&1
-    fi
-    cd /usr/local/bin && sudo rm $COIN_CLI $COIN_DAEMON > /dev/null 2>&1 && cd
-    echo -e "${GREEN}* Done${NONE}";
+  rm $COIN_CLI $COIN_DAEMON > /dev/null 2>&1
+  if [ -d "~/.$COIN_NAME" ]; then
+  sudo rm -rf ~/.$COIN_NAME > /dev/null 2>&1
+  fi
+  cd /usr/local/bin && sudo rm $COIN_CLI $COIN_DAEMON > /dev/null 2>&1 && cd
+  echo -e "${GREEN}* Done${NONE}";
 }
 
 function download_node() {
-  echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
-  cd $TMP_FOLDER >/dev/null 2>&1
+  echo -e "Prepare to download ${GREEN}$COIN_NAME${NC}"
+  cd /root >/dev/null 2>&1
   wget -q $COIN_TGZ
   unzip $COIN_ZIP >/dev/null 2>&1
-  cd linux
   chmod +x $COIN_DAEMON
   chmod +x $COIN_CLI
   cp $COIN_DAEMON $COIN_PATH
   cp $COIN_CLI $COIN_PATH
   cd ~ >/dev/null 2>&1
-  rm -rf $TMP_FOLDER >/dev/null 2>&1
+  rm $COIN_ZIP >/dev/null 2>&1
   clear
 }
 
 function configure_systemd() {
-  cat << EOF > /etc/systemd/system/$COIN_NAME.service
+cat << EOF > /etc/systemd/system/$COIN_NAME.service
 [Unit]
 Description=$COIN_NAME service
 After=network.target
@@ -64,7 +58,6 @@ User=root
 Group=root
 
 Type=forking
-#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
 
 ExecStart=$COIN_PATH$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
 ExecStop=-$COIN_PATH$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
@@ -81,14 +74,17 @@ WantedBy=multi-user.target
 EOF
 
   systemctl daemon-reload
-  sleep 3
+  sleep 4
   systemctl start $COIN_NAME.service
   systemctl enable $COIN_NAME.service >/dev/null 2>&1
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
+    echo -e "------------------------------------------------------------------------------------------------------------------------"
     echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
-    echo -e "systemctl status $COIN_NAME.service"
+    echo -e "systemctl start $COIN_NAME"
+    echo -e "systemctl status $COIN_NAME"
+    echo -e "less /var/log/syslog"
+    echo -e "------------------------------------------------------------------------------------------------------------------------"
     exit 1
   fi
 }
@@ -96,38 +92,26 @@ EOF
 function create_config() {
   mkdir $CONFIGFOLDER >/dev/null 2>&1
   RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w15 | head -n1)
-  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
+  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w25 | head -n1)
   cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
 rpcuser=$RPCUSER
 rpcpassword=$RPCPASSWORD
 rpcport=$RPC_PORT
 rpcallowip=127.0.0.1
-port=$COIN_PORT
 listen=1
 server=1
 daemon=1
+addnode=80.211.46.232
+addnode=80.211.150.242
+addnode=80.211.45.91
+addnode=80.211.45.157
+port=$COIN_PORT
 EOF
 }
 
 function create_key() {
-  echo -e "${YELLOW}Enter your ${RED}$COIN_NAME Masternode GEN Key${NC}. Or press enter generate new Genkey"
+  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC} and press Enter:"
   read -e COINKEY
-  if [[ -z "$COINKEY" ]]; then
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-   exit 1
-  fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  if [ "$?" -gt "0" ];
-    then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the GEN Key${NC}"
-    sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  fi
-  $COIN_PATH$COIN_CLI stop
-fi
 clear
 }
 
@@ -137,21 +121,21 @@ function update_config() {
 logintimestamps=1
 maxconnections=256
 masternode=1
-masternodeaddr=$NODEIP:$COIN_PORT
+externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
-addnode=80.211.46.232
-addnode=80.211.150.242
-addnode=80.211.45.91
-addnode=80.211.45.157
 EOF
 }
 
 function enable_firewall() {
-  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
+  echo -e "------------------------------------------------------------------"
+  echo -e "${GREEN}Installing and setting up firewall${NC}"
+  echo -e "------------------------------------------------------------------"
   ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null
+  ufw allow $RPC_PORT/tcp comment "$COIN_NAME MN RPC port" >/dev/null
   ufw allow ssh comment "SSH" >/dev/null 2>&1
-  ufw limit ssh/tcp comment "SSH Limit" >/dev/null 2>&1
+  ufw limit ssh/tcp comment "Limit SSH" >/dev/null 2>&1
   ufw default allow outgoing >/dev/null 2>&1
+  ufw logging on >/dev/null 2>&1
   echo "y" | ufw enable >/dev/null 2>&1
 }
 
@@ -164,11 +148,13 @@ function get_ip() {
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
     then
-      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+      echo -e "-----------------------------------------------------------------------------------------------"
+      echo -e "${RED}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
       INDEX=0
       for ip in "${NODE_IPS[@]}"
       do
         echo ${INDEX} $ip
+      echo -e "-----------------------------------------------------------------------------------------------"  
         let INDEX=${INDEX}+1
       done
       read -e choose_ip
@@ -178,51 +164,78 @@ function get_ip() {
   fi
 }
 
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "------------------------------------------------------------------"
+  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  echo -e "------------------------------------------------------------------"
+  exit 1
+fi
+}
+
 function checks() {
 if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  echo -e "------------------------------------------------------------------------------"
+  echo -e "${RED}You are not running Ubuntu 16.04. Why? Installation is cancelled.${NC}"
+  echo -e "------------------------------------------------------------------------------"
   exit 1
 fi
 
 if [[ $EUID -ne 0 ]]; then
+   echo -e "------------------------------------------------------------------"
    echo -e "${RED}$0 must be run as root.${NC}"
+   echo -e "------------------------------------------------------------------"
    exit 1
 fi
 
 if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
-  echo -e "${RED}$COIN_NAME is already installed.${NC} Run again to re-install"
+  echo -e "-----------------------------------------------------------------------------------"
+  echo -e "${RED}$COIN_NAME masternode is already installed! Installation is cancelled.${NC}"
+  echo -e "-----------------------------------------------------------------------------------"
   exit 1
 fi
 }
 
 function prepare_system() {
-echo -e "Preparing the VPS to setup. ${CYAN}$COIN_NAME${NC} ${RED}Masternode${NC}"
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
+echo -e "-----------------------------------------------------------------------"
+echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node"
+echo -e "Loading updates for Ubuntu, installing tools..."
+echo -e "Please be patient and wait a moment..."
+echo -e "-----------------------------------------------------------------------"
 apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" sudo git wget curl ufw unzip htop >/dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" sudo git wget curl ufw fail2ban nano unzip htop >/dev/null 2>&1
+export LC_ALL="en_US.UTF-8" >/dev/null 2>&1
+export LC_CTYPE="en_US.UTF-8" >/dev/null 2>&1
+locale-gen --purge >/dev/null 2>&1
+
+apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* && rm gossip-ubuntu16.zip
+wget https://github.com/smai2018/GOSSIP-masternode-autoinstall/raw/master/gos-control.sh && chmod +x gos-control.sh
 if [ "$?" -gt "0" ];
   then
+    echo -e "----------------------------------------------------------------------------------------------------------------------------------"
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
     echo "apt-get update"
-    echo "apt install -y git curl wget ufw unzip htop"
+    echo "apt -y install sudo git wget curl ufw fail2ban nano unzip htop"
+    echo -e "wget https://github.com/smai2018/GOSSIP-masternode-autoinstall/raw/master/gos-control.sh"
+    echo -e "----------------------------------------------------------------------------------------------------------------------------------"
  exit 1
 fi
 clear
 }
 
 function important_information() {
- echo
- echo -e "${BLUE}================================================================================================================================${NC}"
- echo -e "${BLUE}================================================================================================================================${NC}"
- echo -e "$COIN_NAME Masternode is up and running listening on port ${GREEN}$COIN_PORT${NC}."
+ echo -e "================================================================================================================================"
+ echo -e "${GREEN}$COIN_NAME Masternode is up and running${NC}."
  echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
- echo -e "Start: ${RED}systemctl start $COIN_NAME.service${NC}"
- echo -e "Stop: ${RED}systemctl stop $COIN_NAME.service${NC}"
- echo -e "VPS_IP:PORT ${GREEN}$NODEIP:$COIN_PORT${NC}"
- echo -e "MASTERNODE GENKEY is: ${RED}$COINKEY${NC}"
- echo -e "Please check ${RED}$COIN_NAME${NC} is running with the following command: ${RED}systemctl status $COIN_NAME.service${NC}"
- echo -e "Use ${RED}$COIN_CLI masternode status${NC} to check your MN."
+ echo -e "Start manuell: systemctl start $COIN_NAME"
+ echo -e "Stop manuell: systemctl stop $COIN_NAME"
+ echo -e "VPS_IP:PORT $NODEIP:$COIN_PORT"
+ echo -e "MASTERNODE PRIVATEKEY is: $COINKEY"
+ echo -e "Please check ${RED}$COIN_NAME${NC} daemon is running with the following command: ${RED}systemctl status $COIN_NAME${NC}"
+ echo -e "Use ${RED}./$COIN_CLI masternode status${NC} to check your Masternode status."
+ echo -e "================================================================================================================================"
 }
 
 function setup_node() {
@@ -237,7 +250,7 @@ function setup_node() {
 
 ##### Main #####
 clear
-purgeOldInstallation
+delete_old_installation
 checks
 prepare_system
 download_node
